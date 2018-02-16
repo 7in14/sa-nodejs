4be4e4ad8bed235daafcc7fbc3a7b387bae2ec10
sergey.aleksandrov <sergey.aleksandrov@avanade.com>
2018-02-16 16:08:23 +0100

    add swagger

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const Hapi = require('hapi');
 const Pinger = require('./utils/pinger');
 const mongoController = require('./controllers/mongoController');
 const releighController = require('./controllers/releighController');
+const HapiSwagger = require('hapi-swagger');
+const Inert = require('inert');
+const Vision = require('vision');
 
 const setupServer = () => {
 
@@ -14,9 +17,26 @@ const setupServer = () => {
         port: process.env.PORT || 3000
     });
 
+    const swaggerOptions = {
+        info: {
+            title: 'Test API Documentation',
+            version: '1.0.0',
+        },
+    };
+
+    server.register([
+        Inert,
+        Vision,
+        {
+            'register': HapiSwagger,
+            'options': swaggerOptions
+        }
+    ]);
+
     server.route({
         method: 'GET',
         path: '/ping',
+        config: { tags: ['api'] },        
         handler: function (request, reply) {
 
             const pong = Pinger.sayPong();
@@ -27,6 +47,7 @@ const setupServer = () => {
     server.route({
         method: 'GET',
         path: '/',
+        config: { tags: ['api'] },   
         handler: function (request, reply) {
 
             reply(`Default route`);
@@ -36,39 +57,44 @@ const setupServer = () => {
     server.route({
         method: 'GET',
         path: '/allData',
+        config: { tags: ['api'] },   
         handler: mongoController.getAll
     });
 
     server.route({
         method: 'GET',
         path: '/dataSources',
+        config: { tags: ['api'] },   
         handler: mongoController.dataSources
     });
 
     server.route({
         method: 'GET',
         path: '/dataSource/{id}',
+        config: { tags: ['api'] },   
         handler: mongoController.getById
     });
 
     server.route({
         method: 'PUT',
         path: '/dataSource',
+        config: { tags: ['api'] },   
         handler: mongoController.addNotifier
     });
 
     server.route({
         method: 'DELETE',
         path: '/dataSource/{id}',
+        config: { tags: ['api'] },   
         handler: mongoController.deleteNotifier
     });
 
     server.route({
-       method: 'GET',
-       path: '/releigh/crime',
-       handler: releighController.getCrimeData
+        method: 'GET',
+        path: '/releigh/crime',
+        config: { tags: ['api'] },   
+        handler: releighController.getCrimeData
     });
-
 
     return server;
 };
